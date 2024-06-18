8bd47842253b56a79f1a9bb7cb9ca488102499e3
MaxSM <maxi.sm117@gmail.com>
2024-06-18 12:33:41 -0300

    Add Loading progress

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Html } from "@react-three/drei";
-
-// TODO: Search for loaders
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
+  const { progress } = useProgress();
+
   return (
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">Loading...</div>
+        <div className="w-[10vw] h-[10vw] rounded-full">Loading: {progress} %</div>
       </div>
     </Html>
   );
